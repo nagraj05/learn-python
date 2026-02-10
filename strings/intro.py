7508ec9ae252f4cf38e769ac5b16f970cce524de
Nagraj <ntadingi5@gmail.com>
2026-02-10 08:50:30 +0530

    learn strings

---
+++ strings/intro.py
@@ -0,0 +1,21 @@
+message = "test"
+print(message)
+print(len(message))
+print(message[0:-1])
+print(message[1:])
+print(message.lower())
+print(message.upper())
+print(message.count("t"))
+print(message.find("t"))
+replacedmessage = message.replace("test", "oh boy")
+print(replacedmessage)
+
+name = "Nagraj"
+greetings = "Hello"
+
+new_greetings = f"{greetings}!{name.upper()}"
+print(new_greetings)
+
+print(dir(name))
+
+print(help(str))
