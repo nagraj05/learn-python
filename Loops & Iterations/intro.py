25524853013885421b373a1a650cdcd02fe87f1b
Nagraj <25037872+nagraj05@users.noreply.github.com>
2026-02-22 04:52:40 +0000

    Learn while loop

--- Loops & Iterations/intro.py
+++ Loops & Iterations/intro.py
@@ -24,5 +24,18 @@ nums= [1,2,3,4,5]
 # for i in range(10):
 #     print(i)
 
-for i in range(1, 11):
-    print(i)
+# for i in range(1, 11):
+#     print(i)
+
+x = 0
+
+# while x < 10:
+#     print(x)
+#     x +=1
+
+while x <10:
+    if x == 5:
+        break
+    print (x)
+    x += 1
+   
