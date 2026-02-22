9d51531fa07c7cff06ceb377f721d069c1d34b69
Nagraj <25037872+nagraj05@users.noreply.github.com>
2026-02-22 04:46:03 +0000

    Learn For Loop

---
+++ Loops & Iterations/intro.py
@@ -0,0 +1,28 @@
+nums= [1,2,3,4,5]
+
+# for index, num in enumerate(nums, start=1):
+#     print(num*2, index)
+
+
+# for num in nums:
+#     if num ==3:
+#         print("Found")
+#         break
+#     print(num)
+
+# for num in nums:
+#     if num == 3:
+#         print ("found")
+#         continue
+#     print(num)
+
+
+# for num in nums:
+#     for letter in 'abc':
+#         print(num, letter)
+
+# for i in range(10):
+#     print(i)
+
+for i in range(1, 11):
+    print(i)
