2c219b73be3080061725f047bc4e0966dd19bcfe
Nagraj <ntadingi5@gmail.com>
2026-02-23 11:43:53 +0530

    Write Month year func

--- Functions/intro.py
+++ Functions/intro.py
@@ -1,9 +1,19 @@
-def hello_func(greeting, hi):
-    return f"{greeting} Nagraj f{hi}"
+def hello_func(greeting, name="Nagraj"):
+    return f"{greeting} {name}"
 
 
 # hello_func()
 # hello_func()
 # hello_func()
 # print(hello_func)
-print(hello_func("Hi", "hey"))
+print(hello_func("Hi", "Roy"))
+
+
+def student_info(*args, **kwargs):
+    print(args)
+    print(kwargs)
+
+
+courses = ["Math", "physics"]
+info = {"name": "Nagraj", "age": 29}
+student_info(*courses, **info)
